6c9ad11b34381aa2ad9a9812d7cb20a8fb006c5f
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-10 14:09:12 +0200

    Link und Text

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 sap.ui.define([
 	"sap/m/Link",
 	"sap/m/Text"
-], function (Link) {
+], function (Link, Text) {
 	"use strict";
 
 	new Link({
 		text: "Zu Google",
 		href: "https://google.de/"
-	}).placeAt("content_1");
+	}).placeAt("content");
 
-}, function(Text) {
 	new Text({
-		text: "HALLLO",
-	}).placeAt("content_2");
+		text: "Hallo",
+	}).placeAt("content");
 });
