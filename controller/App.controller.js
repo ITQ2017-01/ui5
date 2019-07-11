ee7b087419c225eefbea4e608adf695f8a7fdd7e
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-11 13:55:12 +0200

    Controller eingefügt

---
+++ controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+	return Controller.extend("hts.itq2017.walkthrough.controller.App", {
+		buttonPress : function () {
+			alert("ALARM!");
+		}
+	});
+});
--- index.js
+++ index.js
@@ -22,19 +22,3 @@ sap.ui.define([
 		text: "Hallo",
 	}).placeAt("content");*/
 });
-
-function buttonPress() {
-	var rand = Math.floor(Math.random()*(5))+1;
-
-	if(rand == 1) {
-		console.log("Der SAP-Guru sagt: 'Ja!'");
-	} else if(rand == 2) {
-		console.log("Der SAP-Guru sagt: 'Wahrscheinlich!'");
-	} else if(rand == 3) {
-		console.log("Der SAP-Guru sagt: 'Vielleicht!'");
-	} else if(rand == 4) {
-		console.log("Der SAP-Guru sagt: 'Wahrscheinlich nicht!'");
-	} else {
-		console.log("Der SAP-Guru sagt: 'Nein!'");
-	}
-}
