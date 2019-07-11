c7b5272d20813af94aa9a3ebe7127f0853c538ca
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-11 14:20:10 +0200

    alert button

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 	return Controller.extend("hts.itq2017.walkthrough.controller.App", {
-		buttonPress : function () {
+		alertButtonPress: function() {
 			alert("ALARM!");
 		}
 	});
