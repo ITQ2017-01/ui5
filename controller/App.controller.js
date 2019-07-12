b27106357c2e05c6b610639d36c5603e9a673009
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-12 10:16:11 +0200

    alert

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
 	"use strict";
 	return Controller.extend("hts.itq2017.walkthrough.controller.App", {
 		alertButtonPress: function() {
 			MessageToast.show("ALARM!");
+			MessageBox.alert("AAL!");
 		}
 	});
 });
