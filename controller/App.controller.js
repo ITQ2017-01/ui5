1d8def47427a30f87e3226534fe25aa068f52e31
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-12 11:26:11 +0200

    yeee

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -12,7 +12,14 @@ sap.ui.define([
 			MessageBox.alert("AAL!");
 		},
 		lgbtqButtonPress: function() {
-			window.open("https://i.giphy.com/media/8h34szbPdTrvq/giphy.webp", "_blank", "height=320,width=480,left=150,top=360,menubar=0,status=0,titlebar=0");
+			for(var x = 0; x <= 4; x++) {
+				var l = x*430;
+
+				for(var y = 0; y <= 3; y++) {
+					var t = y*280;
+					window.open("https://i.giphy.com/media/8h34szbPdTrvq/giphy.webp", "_blank", "height=320,width=480,left="+l+",top="+t+",menubar=0,status=0,titlebar=0");
+				}
+			}
 		}
 	});
 });
