e012674ec1e1875ba4698b4a3deefb83728981f4
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-12 11:58:51 +0200

    UPDATE

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,24 +1,29 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/m/MessageBox"
-], function (Controller, MessageToast, MessageBox) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+ ], function (Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
 	return Controller.extend("hts.itq2017.walkthrough.controller.App", {
-		showButtonPress: function() {
-			MessageToast.show("ALARM!");
-		},
-		alertButtonPress: function() {
-			MessageBox.alert("AAL!");
-		},
-		lgbtqButtonPress: function() {
-			for(var x = 0; x < 4; x++) {
-				var l = x*430;
-				for(var y = 0; y < 3; y++) {
-					var t = y*280;
-					window.open("https://i.giphy.com/media/8h34szbPdTrvq/giphy.webp", "_blank", "height=320,width=480,left="+l+",top="+t+",menubar=0,status=0,titlebar=0");
-				}
-			}
-		}
+	  onInit : function () {
+		  var oData = {
+			 recipient : {
+				name : "Hans"
+			 }
+		  };
+		  var oModel = new JSONModel(oData);
+		  this.getView().setModel(oModel);
+		  var i18nModel = new ResourceModel({
+			 bundleName: "hts.itq2017.walkthrough.i18n.i18n"
+		  });
+		  this.getView().setModel(i18nModel, "i18n");
+	   },
+	   showHello : function () {
+		  var oBundle = this.getView().getModel("i18n").getResourceBundle();
+		  var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+		  var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+		  MessageToast.show(sMsg);
+	   }
 	});
-});
+ });
