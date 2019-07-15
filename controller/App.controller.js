00eff7f984b135b9a14ec5c095130c12c39355df
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-15 13:57:31 +0200

    Button erweitert

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -4,27 +4,33 @@ sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/resource/ResourceModel"
- ], function (Controller, MessageToast, JSONModel, ResourceModel) {
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
 	return Controller.extend("hts.itq2017.walkthrough.controller.App", {
-	  onInit : function () {
-		  var oData = {
-			 recipient : {
-				name : "Hans"
-			 }
-		  };
-		  var oModel = new JSONModel(oData);
-		  this.getView().setModel(oModel);
-		  var i18nModel = new ResourceModel({
-			 bundleName: "hts.itq2017.walkthrough.i18n.i18n"
-		  });
-		  this.getView().setModel(i18nModel, "i18n");
-	   },
-	   showHello : function () {
-		  var oBundle = this.getView().getModel("i18n").getResourceBundle();
-		  var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-		  var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-		  MessageToast.show(sMsg);
-	   }
+		onInit: function() {
+			var oData = {
+				recipient: {
+					name: "Hans",
+					noten: [1, 2, 2, 3]
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+			var i18nModel = new ResourceModel({
+				bundleName: "hts.itq2017.walkthrough.i18n.i18n"
+			});
+			this.getView().setModel(i18nModel, "i18n");
+		},
+		onHello: function() {
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			MessageToast.show(sMsg);
+		},
+		onAlert: function() {
+			MessageBox.alert("ALARM, ALARM!", {
+				title: "ACHTUNG"
+			});
+		}
 	});
- });
+});
