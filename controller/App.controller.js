6b1f8af0c0ff1e7117f7537e552a6a70a7cf6c63
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-15 14:01:32 +0200

    MessageBox Fix

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -2,9 +2,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
+	"sap/m/MessageBox",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+], function (Controller, MessageToast, MessageBox, JSONModel, ResourceModel) {
 	"use strict";
 	return Controller.extend("hts.itq2017.walkthrough.controller.App", {
 		onInit: function() {
