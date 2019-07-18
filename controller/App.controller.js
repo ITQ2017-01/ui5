e8ff41a916c6336e98e224d227ed3abb6c47845b
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-18 12:08:45 +0200

    Walkthrough Update

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,10 +1,6 @@
 /* Module laden */
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"sap/m/MessageBox",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
+	"sap/ui/core/mvc/Controller"
 ], function (Controller, MessageToast, MessageBox, JSONModel, ResourceModel) {
 	"use strict";
 
--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -1,16 +1,15 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+   "sap/m/MessageToast",
+   "sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
    "use strict";
    return Controller.extend("hts.itq2017.demo.walkthrough.controller.HelloPanel", {
       onShowHello : function () {
-         // read msg from i18n model
          var oBundle = this.getView().getModel("i18n").getResourceBundle();
          var sRecipient1 = this.getView().getModel().getProperty("/recipient/given_name");
-		var sRecipient2 = this.getView().getModel().getProperty("/recipient/family_name");
-		var sMsg = oBundle.getText("helloMsg", [sRecipient1, sRecipient2]);
-         // show message
+         var sRecipient2 = this.getView().getModel().getProperty("/recipient/family_name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient1, sRecipient2]);
          MessageToast.show(sMsg);
       },
       onAlert: function() {
